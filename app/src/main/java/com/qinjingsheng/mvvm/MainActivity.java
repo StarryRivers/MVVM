d42157d20100c6d60b49bc1782b9e4a25b49123f
StarryRivers <qinjingsheng@foxmail.com>
2020-11-05 19:07:09 +0800

    Initial Commit

---
+++ app/src/main/java/com/qinjingsheng/mvvm/MainActivity.java
@@ -0,0 +1,20 @@
+package com.qinjingsheng.mvvm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author : StarryRivers
+ * Project  : EveryDay
+ * Desc     : Retrofit 请求工具类，接口代理对象类
+ * @date : 2020/11/5 18:47
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
